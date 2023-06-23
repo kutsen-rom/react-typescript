8a2cf4dc5f1021dea16c49f1dcefc3f8b02075b8
kutsen-rom <62949847+kutsen-rom@users.noreply.github.com>
2023-06-23 14:06:17 +0300

    Complete exercise 24

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import "./App.css";
+import Text from "./component/polymorphic/Text";
 
 function App() {
   return (
     <div className="App">
+      <Text as="h1" size="lg">
+        Heading
+      </Text>
+      <Text as="p" size="md">
+        Paragraph
+      </Text>
+      <Text as="label" htmlFor='someId' size="sm" color="secondary">
+        Label
+      </Text>
     </div>
   );
 }
