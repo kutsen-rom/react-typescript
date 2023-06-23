3177018ba41a23a3e28dfe4666fbb4e22b7e70d6
kutsen-rom <62949847+kutsen-rom@users.noreply.github.com>
2023-06-23 13:11:45 +0300

    Complete exercise 20

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import "./App.css";
-import List from "./component/generics/List";
+import RandomNumber from "./component/restrictions/RandomNumber";
 
 function App() {
   return (
     <div className="App">
-      {/* <List
-        items={["Roman", "Akira", "Diana"]}
-        onClick={(item) => console.log(item)}
-      />
-      <List items={[1, 2, 3]} onClick={(item) => console.log(item)} /> */}
-      <List
-        items={[
-          { id: 1, first: "Roman", last: "Lastname" },
-          { id: 2, first: "Akira", last: "Kurosawa" },
-          { id: 3, first: "Diana", last: "Princess" },
-        ]}
-        onClick={(item) => console.log(item)}
-      />
+      <RandomNumber value={10} isPositive />
     </div>
   );
 }
