03aaace758558537c32759a56a0a0daf9559dfaf
kutsen-rom <62949847+kutsen-rom@users.noreply.github.com>
2023-06-23 11:49:53 +0300

    Complete exercise 18

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import "./App.css";
+import Private from "./component/auth/Private";
+import Profile from "./component/auth/Profile";
 
 function App() {
   return (
     <div className="App">
+      <Private isLoggedIn={false} component={Profile} />
     </div>
   );
 }
